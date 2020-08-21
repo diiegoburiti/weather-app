d5083ce77beb9ac825c5a4cb505b2ba5ba6aaf53
Diego Buriti <bs.diego97@gmail.com>
2020-08-21 18:14:59 -0300

    added ,env file to apy key.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const api = {
+  key: process.env.REACT_APP_KEY,
+  url: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = React.useState("");
+  const [weather, setWeather] = React.useState({});
+
+  function dataBuilder() {
+    let today = new Date();
+    today.toDateString();
+    return `${today} `;
+  }
+
+  function searchWeather(event) {
+    if (event.key === "Enter") {
+      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+    console.log(process.env.REACT_APP_KEY);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={searchWeather}
+          />
+        </div>
+        {weather.main ? (
+          <div className="location-box">
+            <div className="location">
+              {weather.name} {weather.sys.country}
+            </div>
+            <div className="date">{dataBuilder()}</div>
+            <div className="wheather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C </div>
+              <div className="wheather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
