202e219cbe8c6f411dfe7e7854027cec2915232c
Diego Buriti <bs.diego97@gmail.com>
2020-08-24 23:38:41 -0300

    asynchronous fetch.

--- src/App.js
+++ src/App.js
@@ -9,21 +9,60 @@ const api = {
 function App() {
   const [query, setQuery] = React.useState("");
   const [weather, setWeather] = React.useState({});
+  const [error, setError] = React.useState(null);
 
-  function dataBuilder() {
-    let today = new Date();
-    today.toDateString();
-    return `${today} `;
+  function dateBuilder(d) {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
   }
 
-  function searchWeather(event) {
+  async function searchWeather(event) {
+    let response;
     if (event.key === "Enter") {
-      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then((res) => res.json())
-        .then((result) => {
-          setWeather(result);
-          setQuery("");
-        });
+      try {
+        setError(null);
+        response = await fetch(
+          `${api.url}weather?q=${query}&units=metric&APPID=${api.key}`,
+        );
+        response = await response.json();
+        setWeather(response);
+        setQuery("");
+        console.log(response);
+      } catch (err) {
+        setError(err.message);
+        console.log("dasdadsa" + err);
+      } finally {
+        setWeather(response);
+        setError(null);
+      }
     }
   }
 
@@ -35,17 +74,18 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search"
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={(event) => setQuery(event.target.value)}
             value={query}
             onKeyPress={searchWeather}
           />
+          {error && <p>{error}</p>}
         </div>
         {weather.main ? (
           <div className="location-box">
             <div className="location">
               {weather.name} {weather.sys.country}
             </div>
-            <div className="date">{dataBuilder()}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
             <div className="wheather-box">
               <div className="temp">{Math.round(weather.main.temp)}°C </div>
               <div className="wheather">{weather.weather[0].main}</div>
