6f8a3df6759f17067af69941fb1c4004f35f430f
Diego Buriti <bs.diego97@gmail.com>
2021-02-19 14:33:50 -0300

    refactor: remove multiples files

--- src/Components/InfoBox/InfoBox.jsx
+++ src/Components/InfoBox/InfoBox.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import * as S from './style'
+const index = ({ weather }) => {
 
-const index = ({weather}) => {
+    function dateBuilder(date) {
+      let o = new Intl.DateTimeFormat(navigator.locales, {day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
+      return o.format(Date.now());
+    }
   return (
-    <div>
-      
-    </div>
+    <S.Wrapper>
+      <S.Location>
+        {weather.name} {weather.sys.country}
+      </S.Location>
+      <S.Date>{dateBuilder()}</S.Date>
+
+      <S.WeatherBox>
+        <S.Temp>{Math.round(weather.main.temp)}°C</S.Temp>
+        <S.CurrentWeather>{weather.weather[0].main}</S.CurrentWeather>
+      </S.WeatherBox>
+    </S.Wrapper>
   )
 }
 
