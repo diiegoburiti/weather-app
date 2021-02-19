6f7af4e7a4ad91378603a424fe115871ab09f1b6
Diego Buriti <bs.diego97@gmail.com>
2021-02-19 14:47:16 -0300

    refactor: rename and remove styled to a single file Input component

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -1,36 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 
-const SearchBar = styled.input`
-  display: block;
-  width: 100%;
-  padding: 15px;
-  appearance: none;
-  border: none;
-  outline: none;
-  background: rgba(255, 255, 255, 0.5);
-  border-radius: 1rem;
-  box-shadow: 0px 5px rgba(0, 0, 0, 0.2);
-  color: #313131;
-  font-size: 20px;
-  transition: 0.4s ease;
-
-  &:focus {
-    background: rgba(255, 255, 255, 0.75);
-  }
-`;
+import * as S from './style'
 
 const Input = ({ onChange, value, type, onKeyPress, placeholder }) => {
   return (
-    <>
-      <SearchBar
+      <S.InputSearch
         type={type}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
         onKeyPress={onKeyPress}
       />
-    </>
   );
 };
 
